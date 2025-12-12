539dd987ebbbd598c860d3992586fc0ee3c96c57
noagaliki-tech <noagaliki@mail.tau.ac.il>
2025-12-12 18:59:14 +0200

    Update assignment 1.R
    Update assignment 1

--- assignment 1.R
+++ assignment 1.R
@@ -6,10 +6,10 @@
 #### CREATE CSV ####
 # 1. Defining variables
 
-checked_number = c(1, 2, 3, 4, 5, 6)
-gender = c('male', 'female', 'female', 'male', 'else' , 'female')
-age = c(40, 27, 32, 39, 31, 18)
-depression = c(0, 0, 1, 0, 1, 1) # 1 = There is depression, 0 = Doesn't have
+checked_number = 1:6 
+gender = sample(c("male", "female", "else"), size = 6, replace = TRUE)
+age = sample(15:40, size = 6, replace = TRUE)
+depression = rbinom(6, 1, 0.5) # 1 = There is depression, 0 = Doesn't have
 
 # 2. Combine to csv 
 df = data.frame(checked_number, gender, age, depression)
